585bef611a1a31141589216ec55b47089bf261f6
CharlesO55 <charles_mathew_ong@dlsu.edu.ph>
2024-02-13 23:50:40 +0800

    Submission ver

--- Assets/Scripts/TrackedImageObjectHandler.cs
+++ Assets/Scripts/TrackedImageObjectHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,21 @@ using UnityEngine.XR.ARSubsystems;
 public class TrackedImageObjectHandler : MonoBehaviour
 {
     [SerializeField] private GameObject m_SpawnObject;
+    private GameObject m_BeaconRef;
     [SerializeField] private float yOffset = 0;
 
-    private TrackingState m_LastState;
-    private bool m_HasSpawned;
+    //private TrackingState m_LastState;
+    [SerializeField] private Canvas m_WarningAlert;
+    
+    private List<ARTrackedImage> m_ValidImages;
+
+    void Start()
+    {
+        m_BeaconRef = null;
+        m_ValidImages = new();
+        m_WarningAlert.enabled = false;
+    }
+    
     public void OnTrackedImageChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
         foreach(var image in eventArgs.added)
@@ -18,30 +30,8 @@ public class TrackedImageObjectHandler : MonoBehaviour
             Debug.Log("[Added]: " +  image.referenceImage.name + " | Tracking state: " + image.trackingState);
         }
 
-        foreach (var image in eventArgs.updated)
-        {
-            Debug.Log("[Updated]: " + image.referenceImage.name + " | Tracking state: " + image.trackingState);
 
-            //SPAWN THE BEACON
-            if (AgentManager.INSTANCE && !m_HasSpawned)
-            {
-                Debug.Log("Spawned Beacon");
-                m_HasSpawned = true;
-
-                SpawnOnImage(image.transform);
-                AgentManager.INSTANCE.MoveAllAgents(image.transform.position);
-            }
-
-
-            //MOVE PLAYERS ONLY WHEN VISIBLE
-            if (AgentManager.INSTANCE &&
-                image.trackingState == TrackingState.Tracking)
-            {
-                AgentManager.INSTANCE.MoveAllAgents(image.transform.position);
-            }
-
-            m_LastState = image.trackingState;
-        }
+        CheckImages(eventArgs);
 
         foreach (var image in eventArgs.removed)
         {
@@ -50,12 +40,54 @@ public class TrackedImageObjectHandler : MonoBehaviour
     }
 
 
+    private void CheckImages(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+    {
+        bool HasLevelSpawned = AgentManager.INSTANCE != null;
+        m_ValidImages.Clear();
+
+
+        foreach (var image in eventArgs.updated)
+        {
+            Debug.Log("[Updated]: " + image.referenceImage.name + " | Tracking state: " + image.trackingState);
+
+            //PLACE THE BEACON WHEN GAME HAS SPAWNED LEVEL WITH MANAGER
+            if (HasLevelSpawned)
+            {
+                if(m_BeaconRef == null)
+                    SpawnOnImage(image.transform);
+
+                if (image.trackingState == TrackingState.Tracking)
+                {
+                    AgentManager.INSTANCE.MoveAllAgents(image.transform.position);
+
+                    //m_LastState = image.trackingState;
+                    m_BeaconRef.SetActive(true);
+                    m_BeaconRef.transform.SetParent(image.transform, false);
+                    
+                    m_ValidImages.Add(image);
+                    m_WarningAlert.enabled = false;
+
+                    break;
+                }
+            }
+        }
+
+
+        if (HasLevelSpawned && m_ValidImages.Count == 0)
+        {
+            m_WarningAlert.enabled = true;
+
+            m_BeaconRef.SetActive(false);
+            AgentManager.INSTANCE.StopAllAgents();
+        }
+    }
+
     private void SpawnOnImage(Transform transform)
     {
-        GameObject newSpawn = Instantiate(m_SpawnObject);
+        m_BeaconRef = Instantiate(m_SpawnObject);
 
-        newSpawn.transform.SetParent(transform, true);
-        
-        newSpawn.transform.localPosition = Vector3.up * yOffset;
+        m_BeaconRef.transform.SetParent(transform, true);
+
+        m_BeaconRef.transform.localPosition = Vector3.up * yOffset;
     }
 }
